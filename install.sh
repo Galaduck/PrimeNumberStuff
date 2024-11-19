f53304e6b19904e6671792f18a72c4d6bd63ac09
A Person <ai6543r@gmail.com>
2024-11-19 15:47:10 -0500

    Added guides and a new list

--- install.sh
+++ install.sh
@@ -1,4 +1,1 @@
-echo "You should not run shell script without checking first"
-echo "This time is fine tho"
-echo "Make sure you have brew installed, also this is only for mac"
 brew install primesieve
