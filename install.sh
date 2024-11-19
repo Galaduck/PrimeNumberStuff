bfa16f239a8e15dee2f5926e2b163dafc6b44b18
A Person <ai6543r@gmail.com>
2024-11-19 15:42:31 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+echo "You should not run shell script without checking first"
+echo "This time is fine tho"
+echo "Make sure you have brew installed, also this is only for mac"
+brew install primesieve
