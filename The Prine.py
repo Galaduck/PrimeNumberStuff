8995368e6a3a8526716274b4d036401fb2d62100
Aiden <ai6543r@gmail.com>
2025-06-15 11:56:31 -0400

    Counts the amound of primes
    Now you know how many primes were found.

--- The Prine.py
+++ The Prine.py
@@ -3,10 +3,18 @@ import random
 
 attempts = input("How many attempts for a prime would you like? :")
 max = input("What power of 10 do you want all your primes to be under? :")
-
+primecount = 0
 for i in range(int(attempts)):
     MabyePrime = random.randint(1, 10**int(max))
 
     if sympy.isprime(MabyePrime) == True:
         print(MabyePrime)
         print(" ")
+        primecount = primecount + 1
+
+
+if primecount == 0:
+    print("Sorry no primes were found")
+
+else:
+    print(primecount, "primes were found")
