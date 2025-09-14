15c588fe70b22fd3eab17fed1c063ae128891ef3
Aiden <ai6543r@gmail.com>
2025-09-14 11:02:13 -0400

    Changed the The prine 2
    Now it is an industrial prine

---
+++ Prines/Industrial Prine.py
@@ -0,0 +1,39 @@
+#This is a prine to find primes that can be used for stuff like encryption
+
+#! This does not prove that the number is prime it just is very likely
+
+# I am not responsible for the accuracy of the primes from this code
+import random
+
+def power(base, exp, mod) :
+    res = 1
+    base %= mod  
+    
+    while exp > 0:
+        if exp % 2 == 1:
+            res = (res * base) % mod
+            
+        base = (base * base) % mod
+        exp //= 2
+        
+    return res
+
+def prp(base, num):
+    expo = num - 1
+
+    remainder = power(base, expo, num)
+
+    if remainder == 1:
+        return True
+    
+    else:
+        return False
+    
+
+
+prime_length = int(input("How many digits would you like your prime to be. It must be at least 100: "))
+
+if prime_length < 100:
+    print("For industrial primes they must be at least 100 digits so that the nunber is probably prime.")
+
+
